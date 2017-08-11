d164453707d6d873915ec734dd893e10e7a04d70
Draekko <draekko.software@gmail.com>
2017-08-11 12:45:08 -0400

    adding kernel build scripts from https://github.com/jcadduono/android_kernel_samsung_exynos7870 tweaked for A5 (2017)

---
+++ build.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# TWRP kernel for Samsung Exynos 7880 devices build script 
+# Derived from 7870 script by jcadduono
+
+################### BEFORE STARTING ################
+#
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
+#
+# once you've set up the config section how you like it, you can simply run
+# ./build.sh [VARIANT]
+#
+###################### MODELS ######################
+#
+# a5y17lte      = SM-A520 - Samsung Galaxy A5 (2017)
+#
+###################### CONFIG ######################
+
+# root directory of universal7870 kernel git repo (default is this script's location)
+RDIR=$(pwd)
+
+[ "$VER" ] ||
+# version number
+VER=$(cat "$RDIR/VERSION")
+
+# directory containing cross-compile arm64 toolchain
+TOOLCHAIN=$ANDROID_BUILD_HOME/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+
+# amount of cpu threads to use in kernel make process
+THREADS=1
+
+############## SCARY NO-TOUCHY STUFF ###############
+
+ABORT()
+{
+	[ "$1" ] && echo "Error: $*"
+	exit 1
+}
+
+export ARCH=arm64
+export CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu-
+
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+[ "$TARGET" ] || TARGET=twrp
+[ "$1" ] && DEVICE=$1
+[ "$2" ] && VARIANT=$2
+[ "$DEVICE" ] || DEVICE=a5y17lte
+[ "$VARIANT" ] || VARIANT=xx
+
+DEFCONFIG=${TARGET}_defconfig
+DEVICE_DEFCONFIG=device_${DEVICE}_${VARIANT}
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEVICE_DEFCONFIG}" ] ||
+ABORT "Device config $DEVICE_DEFCONFIG not found in $ARCH configs!"
+
+export LOCALVERSION=$TARGET-$DEVICE-$VARIANT-$VER
+
+CLEAN_BUILD()
+{
+	echo "Cleaning build..."
+	cd "$RDIR"
+	rm -rf build
+}
+
+SETUP_BUILD()
+{
+	echo "Creating kernel config for $LOCALVERSION..."
+	cd "$RDIR"
+	mkdir -p build
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		DEVICE_DEFCONFIG="$DEVICE_DEFCONFIG" \
+		|| ABORT "Failed to set up build"
+}
+
+BUILD_KERNEL()
+{
+	echo "Starting build for $LOCALVERSION..."
+	while ! make -C "$RDIR" O=build -j"$THREADS"; do
+		read -p "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+}
+
+BUILD_DTB()
+{
+	echo "Generating dtb.img..."
+	"$RDIR/dtbgen.sh" "$DEVICE" "$VARIANT" || ABORT "Failed to generate dtb.img!"
+}
+
+CLEAN_BUILD && SETUP_BUILD && BUILD_KERNEL && BUILD_DTB && echo "Finished building $LOCALVERSION!"
---
+++ dtbgen.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# simple bash script for generating dtb image
+
+# root directory of universal7880 kernel git repo (default is this script's location)
+RDIR=$(pwd)
+
+# directory containing cross-compile arm64 toolchain
+TOOLCHAIN=$ANDROID_BUILD_HOME/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+
+# device dependant variables
+PAGE_SIZE=2048
+DTB_PADDING=0
+
+export ARCH=arm64
+export CROSS_COMPILE=$TOOLCHAIN/bin/aarch64-linux-gnu-
+
+BDIR=$RDIR/build
+OUTDIR=$BDIR/arch/$ARCH/boot
+DTSDIR=$RDIR/arch/$ARCH/boot/dts
+DTBDIR=$OUTDIR/dtb
+DTCTOOL=$BDIR/scripts/dtc/dtc
+INCDIR=$RDIR/include
+
+ABORT()
+{
+	[ "$1" ] && echo "Error: $*"
+	exit 1
+}
+
+[ -x "$DTCTOOL" ] ||
+ABORT "You need to run ./build.sh first!"
+
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+[ "$1" ] && DEVICE=$1
+[ "$2" ] && VARIANT=$2
+
+case $DEVICE in
+a5y17lte)
+	case $VARIANT in
+	xx|can)
+		DTSFILES="exynos7880-j7xelte_can_open_00 exynos7880-j7xelte_can_open_01
+			exynos7880-j7xelte_can_open_02 exynos7880-j7xelte_can_open_03
+			exynos7880-j7xelte_can_open_05 exynos7880-j7xelte_can_open_08
+			exynos7880-j7xelte_can_open_08"
+	xx|eur)
+		DTSFILES="exynos7880-j7xelte_eur_open_00 exynos7880-j7xelte_eur_open_01
+			exynos7880-j7xelte_eur_open_02 exynos7880-j7xelte_eur_open_03
+			exynos7880-j7xelte_eur_open_05 exynos7880-j7xelte_eur_open_08
+			exynos7880-j7xelte_eur_open_08"
+		;;
+	kor|ktt|lgt|skt)
+		DTSFILES="exynos7880-j7xelte_kor_03 exynos7880-j7xelte_kor_04"
+		;;
+	*) ABORT "Unknown variant of $DEVICE: $VARIANT" ;;
+	esac
+	DTBH_PLATFORM_CODE=0x50a6
+	DTBH_SUBTYPE_CODE=0x217584da
+	;;
+*) ABORT "Unknown device: $DEVICE" ;;
+esac
+
+mkdir -p "$OUTDIR" "$DTBDIR"
+
+cd "$DTBDIR" || ABORT "Unable to cd to $DTBDIR!"
+
+rm -f ./*
+
+echo "Processing dts files..."
+
+for dts in $DTSFILES; do
+	echo "=> Processing: ${dts}.dts"
+	"${CROSS_COMPILE}cpp" -nostdinc -undef -x assembler-with-cpp -I "$INCDIR" "$DTSDIR/${dts}.dts" > "${dts}.dts"
+	echo "=> Generating: ${dts}.dtb"
+	$DTCTOOL -p $DTB_PADDING -i "$DTSDIR" -O dtb -o "${dts}.dtb" "${dts}.dts"
+done
+
+echo "Generating dtb.img..."
+"$RDIR/scripts/dtbTool/dtbTool" -o "$OUTDIR/dtb.img" -d "$DTBDIR/" -s $PAGE_SIZE --platform $DTBH_PLATFORM_CODE --subtype $DTBH_SUBTYPE_CODE || exit 1
+
+echo "Done."
